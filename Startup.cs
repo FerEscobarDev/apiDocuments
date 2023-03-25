783b0634254b8d25aebb198f093c3253dea2f7cb
Fernando Escobar <jeiesc04@outlook.com>
2023-03-24 23:44:45 -0500

    Add Class Startup
    Startup class is added, services and Http configuration are configured.

---
+++ Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace apiDocuments
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) //constructor
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; } //Define propiedad configuration
+
+        public void ConfigureServices(IServiceCollection services) //Se configuran los servicios
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
