f6721951ba56045c665e2757a92e0339f319f7fd
Fernando Escobar <jeiesc04@outlook.com>
2023-03-25 01:22:06 -0500

    Add folder Models and Class Document

---
+++ Models/Document.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apiDocuments.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        [Required]
+        public string CustomName { get; set; }
+        [Required]
+        public string OriginalName { get; set; }
+        [Required]
+        public string Extension { get; set; }
+        [Required]
+        public string MimeType { get; set; }
+        [Required]
+        public string DocumentUrl { get; set; }
+    }
+}
