e7a1a9b4a3c1244de721fd24b8b7f3edfe221df7
Fernando Escobar <jeiesc04@outlook.com>
2023-03-24 23:57:40 -0500

    Add DbContex
    Added DbContext class for database administration and table creation based on Models.

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace apiDocuments
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //Aquí van los modelos de las tablas que se van a crear
+        //public DbSet<NombreModelo> NombreModeloPlural { get; set; }
+    }
+}
