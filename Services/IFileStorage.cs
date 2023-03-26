eb4a42fdeed6d4b607dd2756c00f613bae17a49f
Fernando Escobar <jeiesc04@outlook.com>
2023-03-26 01:15:17 -0500

    Add IFileStorage and LocalFileStorage
    interface and class for file management, saving, editing, deletingadded

---
+++ Services/IFileStorage.cs
@@ -0,0 +1,11 @@
+﻿namespace apiDocuments.Services
+{
+    public interface IFileStorage
+    {
+        Task<string> SaveFile(byte[] data, string extension, string container, string contentType);
+
+        Task<string> EditFile(byte[] data, string extension, string container, string path, string contentType);
+
+        Task DeleteFile(string path, string container);
+    }
+}
---
+++ Services/LocalFileStorage.cs
@@ -0,0 +1,56 @@
+﻿namespace apiDocuments.Services
+{
+    public class LocalFileStorage : IFileStorage
+    {
+        private readonly IWebHostEnvironment env; //Para obtener ruta wwwroot
+        private readonly IHttpContextAccessor httpContextAccessor; //Para determinar dominio y poder crear url documento
+
+        public LocalFileStorage(IWebHostEnvironment env, IHttpContextAccessor httpContextAccessor)
+        {
+            this.env = env;
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public Task DeleteFile(string path, string container)
+        {
+            if (path != null)
+            {
+                var fileName = Path.GetFileName(path);
+                string pathFile = Path.Combine(env.WebRootPath, container, fileName);
+
+                if (File.Exists(pathFile))
+                {
+                    File.Delete(pathFile);
+                }
+            }
+
+            return Task.FromResult(0);
+        }
+
+        public async Task<string> EditFile(byte[] data, string extension, string container, string path, string contentType)
+        {
+            await DeleteFile(path, container);
+            return await SaveFile(data, extension, container, contentType);
+        }
+
+        public async Task<string> SaveFile(byte[] data, string extension, string container, string contentType)
+        {
+            var fileName = $"{Guid.NewGuid()}{extension}";
+            string folder = Path.Combine(env.WebRootPath, container);
+
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            string path = Path.Combine(folder, fileName);
+            await File.WriteAllBytesAsync(path, data);
+
+            var currentPath = $"{httpContextAccessor.HttpContext.Request.Scheme}://{httpContextAccessor.HttpContext.Request.Host}";
+
+            var urlDb = Path.Combine(currentPath, container, fileName).Replace("\\", "/");
+
+            return urlDb;
+        }
+    }
+}
