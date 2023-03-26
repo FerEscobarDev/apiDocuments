7873515f3293c35a767d3462675882acc455c149
Fernando Escobar <jeiesc04@outlook.com>
2023-03-26 01:11:26 -0500

    Add  Validator MaxSizeFile
    validator maximum file size

---
+++ Validations/MaxSizeFile.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apiDocuments.Validations
+{
+    public class MaxSizeFile: ValidationAttribute
+    {
+        private readonly int maxSizeMb;
+
+        public MaxSizeFile(int MaxSizeMb)
+        {
+            maxSizeMb = MaxSizeMb;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return ValidationResult.Success;
+            }
+
+            IFormFile formFile = value as IFormFile;
+
+            if (formFile == null) 
+            {
+                return ValidationResult.Success;
+            }
+
+            if(formFile.Length > maxSizeMb * 1024 * 1024)
+            {
+                return new ValidationResult($"El peso del archivo no debe ser mayor a {maxSizeMb}Mb");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
