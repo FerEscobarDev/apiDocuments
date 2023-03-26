da9ec65bd0020101e3e127856b1d499356c44b6b
Fernando Escobar <jeiesc04@outlook.com>
2023-03-26 01:19:37 -0500

    Add AutoMapperProfile
    AutoMapperProfile class for Model to DTO and DTO to Model conversion.

---
+++ Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,15 @@
+﻿using apiDocuments.DTOs;
+using apiDocuments.Models;
+using AutoMapper;
+
+namespace apiDocuments.Helpers
+{
+    public class AutoMapperProfiles: Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Document, DocumentDTO>().ReverseMap();
+            CreateMap<DocumentCreateDTO, Document>().ForMember(x => x.DocumentFile, options => options.Ignore());
+        }
+    }
+}
