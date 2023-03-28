5d5ecd160318634a78ac8ddb36197a5a4c6cb013
Fernando Escobar <jeiesc04@outlook.com>
2023-03-28 12:07:48 -0500

    Add FilterDocumentsDTO class
    DTO class added for filter implementation

---
+++ DTOs/FilterDocumentsDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apiDocuments.DTOs
+{
+    public class FilterDocumentsDTO
+    {
+        public string CustomName { get; set; }
+        public string OriginalName { get; set; }
+        public string Extension { get; set; }
+        public string MimeType { get; set; }
+    }
+}
